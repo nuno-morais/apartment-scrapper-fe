a5a0409aef4195a0376d33f4962b781a61b6eda2
Nuno Morais <nram@nuno-morais.eu>
2020-01-23 00:21:18 +0000

    Improve url on Links page

--- src/links/links.js
+++ src/links/links.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { List, Datagrid, TextField, TextInput, Create, SimpleForm } from 'react-admin';
+import PropTypes from 'prop-types';
 
-export const LinkList = props => (
-    <List {...props}>
-        <Datagrid >
-            <TextField source="url" />
-            <TextField source="provider" />
-            <TextField source="createdAt" />
-            <TextField source="updatedAt" />
-        </Datagrid>
-    </List>
-);
+const AmazingUrlField = ({ source, record = {} }) => {
+    const appendTitle = record.url.length > 100 ? '...' : '';
 
+    const title = record.url.substring(0, 100) + appendTitle;
+    return (<a target={'_blank'} href={record.url} >{title}</a>)
+}
+
+export const LinkList = props => {
+    const {record} = props;
+    return (
+        <List {...props}>
+            <Datagrid >
+                <AmazingUrlField record={record} source="url" />
+                <TextField source="provider" />
+                <TextField source="createdAt" />
+            </Datagrid>
+        </List>
+    );
+}
 
 export const LinkCreate = (props) => (
     <Create {...props}>
